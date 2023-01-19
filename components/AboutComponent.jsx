cb993a141af46ba3da6dd209413b41897250bc48
fandres51 <fabio.and1514@gmail.com>
2023-01-19 18:40:48 -0500

    typos

--- components/AboutComponent.jsx
+++ components/AboutComponent.jsx
@@ -8,9 +8,9 @@ export default function AboutComponent({ title }) {
       <div className={styles.title}>{title}</div>
       <div className={styles.text}>🎓 I graduated in 2021.</div>
       <div className={styles.text}>💻 While at the university, I worked in some different intership jobs.</div>
-      <div className={styles.text}>🐍 My knowledge includes javascript (React, R. Native, Node) and python.</div>
+      <div className={styles.text}>🐍 My knowledge includes JavaScript (React, React Native, NodeJS), python, and UX design.</div>
       <div className={styles.text}>✏️ Then I decided to take a career break, so I started to learn publicity.</div>
-      <div className={styles.text}>💡 Now I&apos;m back to code but using my other, more creative side, for all my work.</div>
+      <div className={styles.text}>💡 Now I&apos;m back to code and UX design using creativity and reasoning in my work.</div>
       <div className={styles.text}>✈️ In other aspects of my life, I love to travel and discover new places and people. My dream is to visit as many places as I can, maybe while working.</div>
     </div>
   )
